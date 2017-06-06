0780925426d7f6c3fec56b6e1e3135d1b4b46c83
AdrienSldy <a.sasia.ldy@cogiway.com>
2017-06-06 14:13:56 +0200

    Game Component
    Structure main component

--- smash_lab/src/component/Main.js
+++ smash_lab/src/component/Main.js
@@ -1,48 +1,20 @@
 import React, { Component } from 'react';
-import Question from './Main/Question'
-import Answers from './Main/Answers';
+import Game from './Main/Game';
 import './Main/Main.css';
 
 export default class Main extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            question: '',
-            answers: []
-        };
-        this.getQuestion = this.getQuestion.bind(this);
-        this.sendAnswer = this.sendAnswer.bind(this);
-        this.getQuestion('player_1');
-    }
-
-    getQuestion(){
-        this.props.getQuestion().then((data) => {         
-            this.setState({
-                'question': data.question,                 
-                'answers': data.answers,
-                'correct_answer': data.correct_answer 
-            });
-        });
-    }
-
-    sendAnswer(index){
-        const ennemy = (this.props.player === 'player_1')?'player_2':'player_1';
-        if(index === this.state.correct_answer){
-            this.props.attack(ennemy);
-        }else{
-            this.props.attack(this.props.player);
-        }
-        this.getQuestion();
+        this.getQuestion = this.props.getQuestion.bind(this);
+        this.attack = this.props.attack.bind(this);
     }
 
     render() {  
-        const question = this.state.question;
-        const answers = this.state.answers;
         return (
-            <div>
-                <Question question={question} />
-                <Answers sendAnswer={this.sendAnswer} answers={answers} />
-            </div>
+            <main>
+                <Game player='player_1' attack={this.attack} getQuestion={this.getQuestion}/>
+                <Game player='player_2' attack={this.attack} getQuestion={this.getQuestion}/>
+            </main>
         )
     }
 }
